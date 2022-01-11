c53b3a0433bd929373742ac2389173d7a4e8d0b7
sysDev-Mag <sergiomagno@outlook.com>
2022-01-11 17:05:45 -0300

    Criado a cobrinha.

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ let canvas = document.getElementById("snake");
 // Setando o contexto do desenho em 2d. Opções são 2d e 3d.
 let context = canvas.getContext('2d');
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
     // método para desenhar um triangulo, (x, y, width, height)
     context.fillRect(0, 0, 16 * box, 16 * box);
+    
+}
+
+function criarCobrinha() {
+    for (let i = 0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);        
+    }
 }
 
 criarBG();
+criarCobrinha();
