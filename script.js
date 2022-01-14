166206706130d73b1dcc9893e57498188a4957fd
sysDev-Mag <sergiomagno@outlook.com>
2022-01-14 14:55:01 -0300

    Movimentos da cobrinha definido.

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ snake[0] = {
     y: 8 * box
 }
 
+let direction = "right";
+
 function criarBG() {
     context.fillStyle = "lightgreen";
     // método para desenhar um triangulo, (x, y, width, height)
@@ -23,5 +25,27 @@ function criarCobrinha() {
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+    
+}
+
+let jogo = setInterval(iniciarJogo, 100);
