730a4e0684585e124d22c77abfbb13a1f0144103
sysDev-Mag <sergiomagno@outlook.com>
2022-01-14 16:45:04 -0300

    Criado a aleatoriedade da comidinha e condição de crescimento da cobrinha

--- script.js
+++ script.js
@@ -59,7 +59,13 @@ function iniciarJogo() {
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
@@ -70,4 +76,4 @@ function iniciarJogo() {
 
 }
 
-let jogo = setInterval(iniciarJogo, 500);
+let jogo = setInterval(iniciarJogo, 100);
